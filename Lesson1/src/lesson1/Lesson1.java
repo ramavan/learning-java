af52460b27d194faf048bd9b293cd82321833ad2
Artem <Artem@Artem-ASUS>
2013-10-21 21:20:27 +0400

    first re;ease

---
+++ Lesson1/src/lesson1/Lesson1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lesson1;
+
+/**
+ *
+ * @author Artem
+ */
+public class Lesson1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello !");
+        // TODO code application logic here
+    }
+}
