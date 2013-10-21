6cfabeb2a6658dd6a4c648636fb9a73bab8eebdc
Artem <Artem@Artem-ASUS>
2013-10-21 22:24:24 +0400

    first re;ease

---
+++ Lesson1/src/external1/Ext1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package external1;
+
+import lesson1.Lesson1;
+
+/**
+ *
+ * @author Artem
+ */
+public class Ext1 {
+    void test() {
+       
+    }
+}
--- Lesson1/src/lesson1/Lesson1.java
+++ Lesson1/src/lesson1/Lesson1.java
@@ -3,18 +3,62 @@
  * and open the template in the editor.
  */
 package lesson1;
+// inner
+
+
+class Computer {
+    
+    /**
+     * Объем памяти в байтах
+     */
+    int ram;
+    final double cpuFreq;
+    boolean isHdmi;
+    static int num=0;
+    /**
+     * Конструктор по умолчанию
+     */
+    public Computer(double fr) {
+        cpuFreq = fr;
+        num++;
+    }
+    
+    void test() {
+       System.out.println("Test: "+cpuFreq);
+    }
+    
+    static void test2() {
+       System.out.println("Количество: " + num);
+    } 
+    
+    static double mean(double[] vs) {
+        ///.......
+        return 0;
+    }
+}
 
 /**
- *
+ *  Мой сурер класс
  * @author Artem
  */
 public class Lesson1 {
-
+    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello !");
-        // TODO code application logic here
+        Computer c1 = new Computer(1.1);
+        Computer.test2();
+
+        Computer c2 = new Computer(1.4);
+        
+        c1.test();
+       
+        
+        c2.test();
+        Computer.test2();
+        
+        System.out.println(3 == Computer.mean(new double[] {1,2,3,4,5}));
     }
 }
